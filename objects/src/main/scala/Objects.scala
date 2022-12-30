221f007ffa8aa5e774a475e0905e1069e74be019
mikedr <derosamiguel@gmail.com>
2022-12-29 22:10:59 -0300

    Class Person

--- objects/src/main/scala/Objects.scala
+++ objects/src/main/scala/Objects.scala
@@ -4,7 +4,13 @@ object Objects extends App{
     val N_EYES = 2
     def canFly: Boolean = false
   }
+  class Person(val name: String) {
+
+  }
 
   println(Person.N_EYES)
   println(Person.canFly)
+
+  val robert = new Person("Robert")
+  println(robert.name)
 }
